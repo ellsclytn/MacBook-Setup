373fc8ff87cbf2942a77bbcf5c1a2eac65c394f0
Ellis Clayton <ellis@ellis.codes>
2024-11-10 21:07:19 +1100

    wip

--- config/fish/linux.conf.d/startx.fish
+++ config/fish/linux.conf.d/startx.fish
@@ -1,5 +1,6 @@
 if status is-login
     if test -z "$DISPLAY" -a -z "$INITIAL_SETUP" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
+        # exec sway -- -keeptty
+        # exec startx -- -keeptty
     end
 end
